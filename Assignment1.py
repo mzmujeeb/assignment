7a7c0029b22678ba658e825417a88c9f3627cd13
Mozibur Rahman <112144739+mzmujeeb@users.noreply.github.com>
2022-09-09 03:17:19 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,57 @@
+#1Write a command to get the Python version you are using.
+
+import sys
+print (sys.version)
+
+#2. Write a python script to print Hello Python on the screen directly in the command line.
+
+print("Hello Python")
+
+#3. Write a python script and store it in a file and execute the file from the command line.
+
+#output   C:\Users\HP\Desktop>python py_version.py    3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)]
+
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+
+print("Hello Python")
+
+#5. Write a python script to print Hello on the first line and Python on the second line.
+
+print("Hello")
+print("Python")
+ #2n method.
+print("Hello","Python",sep='\n')
+
+#6. Write a python script to print “MySirG” on the screen.
+
+print("\"MySirGi\"")
+
+#7. Write a python script to print “Teacher’s Day” on the screen
+print("\"Teacher's Day\"")
+
+#8. Write a python script to print the value of a variable. Variable contains your name as data.
+x= "Mujeeb Rahman"
+print(x)
+
+
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+
+Name= "Mujeeb Rahman"
+print(x)
+age= 19
+print(age)
+Qualification= "B.tech"
+print (Qualification)
+Experience= "Fresher"
+print (Experience)
+ #2nd method.
+print(Name,age,Qualification,Experience,sep='\n')
+
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values.
+
+a=12
+b=24
+c=19
+print(a)
+print(b)
+print(c)
